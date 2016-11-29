57297dc8cffa80527d8441204f9ef5b986b6d7a8
Norbert Varga <norbert.varga@emarsys.com>
2016-11-29 18:01:11 +0100

    Update Item model
    Add default false value for is_completed.
    Cast is_completed as boolean.

--- app/models/Item.php
+++ app/models/Item.php
@@ -1,11 +1,26 @@
 <?php
 
-namespace App;
+namespace App\models;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Item extends Model
 {
-    //
+    /**
+     * @inheritdoc
+     */
+    protected $attributes = [
+        'is_completed' => false
+    ];
+    /**
+     * @inheritdoc
+     */
+    protected $casts = [
+        'is_completed' => 'boolean',
+    ];
+
+    /**
+     * @inheritdoc
+     */
     protected $fillable = ['title', 'is_completed'];
 }
